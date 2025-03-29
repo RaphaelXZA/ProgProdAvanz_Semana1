7d1f81274ddfb81f17e5eda63532cdb3174668a0
RaphaelXZA <108958428+RaphaelXZA@users.noreply.github.com>
2025-03-29 03:48:53 -0500

    Constructor

--- ProgProdAvanz_Semana1/GameEntity.cs
+++ ProgProdAvanz_Semana1/GameEntity.cs
@@ -11,6 +11,12 @@ namespace ProgProdAvanz_Semana1
         public string Name { get; protected set; }
         public string Description { get; protected set; }
 
+        protected GameEntity(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
         public abstract string Interact();
 
     }
